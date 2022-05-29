9cd6c0a1b56236b1331025bf6835d94ba58a8a24
dsigaran5 <dsigaran1946@gmail.com>
2022-05-29 11:13:33 -0600

    Cambio de texto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola denis
+          Hola Hector
         </p>
         <a
           className="App-link"
